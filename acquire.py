22db9018cef66d9ebf2c44469a2dafb9d5404bc5
Nomer Evangelista <polo239239@gmail.com>
2023-11-24 13:48:49 -0600

    adding acquire.ipynb and acquire.py file in Github

---
+++ acquire.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+import seaborn as sns
+import os
+import env
+
+def check_file_exists(filename, query, url):
+    if os.path.exists(filename):
+        print('this file exists, reading from csv file')
+    else:
+        print('this file does not exist, read from sql, and export to csv file')
+    df = pd.read_sql(query, url)
+    df.to_csv(filename)
+    
+    return df
+
+# caching titanic_db
+def get_titanic_db():
+    url = env.get_db_url('titanic_db')
+    query = 'SELECT * FROM passengers'
+    
+    filename = 'titanic_csv'
+    
+    df = check_file_exists(filename, query, url)
+    
+    return df
+
+# caching iris_db
+def get_iris_db():
+    url = env.get_db_url('iris_db')
+    
+    query = '''SELECT *
+            FROM species
+                JOIN measurements
+                    USING (species_id)'''
+                    
+    filename = 'iris_csv'
+    
+    df = check_file_exists(filename, query, url)
+    
+    return df
+
+# caching telco_db
+def get_telco_db():
+    url = env.get_db_url('telco_churn')
+    
+    query = '''SELECT * 
+            FROM customers
+                JOIN contract_types
+                    USING (contract_type_id)
+                JOIN internet_service_types
+                    USING (internet_service_type_id)
+                JOIN payment_types
+                    USING (payment_type_id)'''
+                    
+    filename = 'telco_csv'
+    
+    df = check_file_exists(filename, query, url)
+    
+    return df
